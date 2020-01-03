093970849c85c993a99d6b095945ad68ff7b64d8
DB <rkdrbgks22@ks.ac.kr>
2020-01-03 16:08:38 +0900

    [ADD] User 번호를 입력받아 번호 List 를 생성

--- src/main/java/domain/User.java
+++ src/main/java/domain/User.java
@@ -3,4 +3,9 @@ package domain;
 import java.util.List;
 
 public class User {
+    private List<Integer> numbers;
+
+    public User(List<Integer> numbers) {
+        this.numbers = numbers;
+    }
 }
