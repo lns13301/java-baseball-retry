9c80792d8b8fd439dd3eecd7c560c7117ae4049a
lns13301 <rkdrbgks22@ks.ac.kr>
2020-01-05 12:57:01 +0900

    [ADD] 입력된 문자열의 정수형 여부를 판별하는 예외 처리

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,5 +1,6 @@
 package view;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class InputView {
@@ -8,5 +9,11 @@ public class InputView {
     public static void InputNumbers() {
         System.out.println("숫자를 입력해주세요.");
         String value = scanner.nextLine();
+
+        try {
+            int number = Integer.parseInt(value);
+        } catch (NumberFormatException e) {
+            System.out.println("정수형 숫자가 아닙니다.");
+        }
     }
 }
