4097f55aeb57172e52f222cc72c3810cb4da45a1
lns13301 <rkdrbgks22@ks.ac.kr>
2020-01-05 12:38:26 +0900

    [ADD] 스트라이크의 개수를 세어 반환하는 기능

--- src/main/java/domain/Computer.java
+++ src/main/java/domain/Computer.java
@@ -1,8 +1,10 @@
 package domain;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Computer {
+    private static final int START = 0;
     private Calculator calculator = new Calculator();
     private List<Integer> numbers;
 
@@ -12,5 +14,13 @@ public class Computer {
 
     public int countBall(List<Integer> numbers) {
         return (int) this.numbers.stream().filter(numbers::contains).count();
+    }
+
+    public int countStrike(List<Integer> numbers) {
+        return IntStream
+                .range(START, numbers.size()).filter(idx -> this.numbers.get(idx)
+                .equals(numbers.get(idx)))
+                .findFirst()
+                .orElse(numbers.size());
     }
 }
