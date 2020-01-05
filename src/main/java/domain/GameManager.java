7b371b533acd45233164c705ee39791adb1a68fd
lns13301 <rkdrbgks22@ks.ac.kr>
2020-01-05 13:31:23 +0900

    [ADD] 게임 종료 시 게임을 계속할 것인지 여부를 선택하는 기능
    - InputNumbers 를 inputNumbers 로 수정

--- src/main/java/domain/GameManager.java
+++ src/main/java/domain/GameManager.java
@@ -8,13 +8,13 @@ import java.util.List;
 public class GameManager {
     private static final int WIN = 3;
 
-    public void play() {
+    public static void play() {
         List<Integer> numbers;
         int strike;
 
         Computer computer = new Computer();
         do {
-            numbers = InputView.InputNumbers();
+            numbers = InputView.inputNumbers();
             strike  = computer.countStrike(numbers);
             OutputView.showResult(strike, computer.countBall(numbers, strike));
         } while (strike == WIN);
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class InputView {
     private static Scanner scanner = new Scanner(System.in);
 
-    public static List<Integer> InputNumbers() {
+    public static List<Integer> inputNumbers() {
         System.out.print("숫자를 입력해주세요 : ");
         String value = scanner.nextLine();
 
@@ -18,6 +18,19 @@ public class InputView {
         } catch (NumberFormatException e) {
             System.out.println("정수형 숫자가 아닙니다.");
         }
-        return InputNumbers();
+        return inputNumbers();
+    }
+
+    public static int inputMenu() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임종료");
+        System.out.println("게임을 새로 시작하시려면 1, 종료하시려면 2를 입력하세요.");
+        String value = scanner.nextLine();
+
+        try {
+            return Integer.parseInt(value);
+        } catch (NumberFormatException e) {
+            System.out.println("정수형 숫자가 아닙니다.");
+        }
+        return inputMenu();
     }
 }
