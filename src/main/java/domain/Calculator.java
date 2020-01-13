813bc557438d3c683d29e546df1610562044f27c
DB <rkdrbgks22@ks.ac.kr>
2020-01-13 13:08:51 +0900

    [REFACTOR] Calculator 를 static 으로 변경하여 코드 간결화
    - package-private 으로 변경

--- src/main/java/domain/Calculator.java
+++ src/main/java/domain/Calculator.java
@@ -4,12 +4,12 @@ import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
 
-public class Calculator {
+class Calculator {
     private static final int MIN = 1;
     private static final int COUNT = 3;
     private static final int MAX = 10;
 
-    public List<Integer> getRandomNumbers() {
+    static List<Integer> getRandomNumbers() {
         return ThreadLocalRandom.current().ints(MIN, MAX).distinct().limit(COUNT).boxed().collect(Collectors.toList());
     }
 }
--- src/main/java/domain/Computer.java
+++ src/main/java/domain/Computer.java
@@ -3,20 +3,19 @@ package domain;
 import java.util.List;
 import java.util.stream.IntStream;
 
-public class Computer {
+class Computer {
     private static final int START = 0;
-    private Calculator calculator = new Calculator();
     private List<Integer> numbers;
 
-    public Computer() {
-        this.numbers = calculator.getRandomNumbers();
+    Computer() {
+        this.numbers = Calculator.getRandomNumbers();
     }
 
-    public int countBall(List<Integer> numbers, int strikeCount) {
+    int countBall(List<Integer> numbers, int strikeCount) {
         return (int) this.numbers.stream().filter(numbers::contains).count() - strikeCount;
     }
 
-    public int countStrike(List<Integer> numbers) {
+    int countStrike(List<Integer> numbers) {
         return (int) IntStream
                 .range(START, 3)
                 .filter(idx -> this.numbers.get(idx).equals(numbers.get(idx)))
